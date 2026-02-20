196d0e38c3be180ae0567bfd35c7928dbcd6768a
cyrillay <cyril.lay.pro@gmail.com>
2026-02-20 08:48:34 +0100

    Fix /metrics with multi worker (#681)
    * Fix /metrics with multi worker
    * Update pyproject.toml version
    * Update unit coverage badge
    * Test CI deployment
    * Update pyproject.toml version
    * Put prometheus multi proc env var inside Dockerfile instead of startup_api.sh
    * Update pyproject.toml version
    * Update unit coverage badge
    
    ---------
    
    Co-authored-by: Cyril LAY <cyril.lay.ext@mail.numerique.gouv.fr>
    Co-authored-by: GitHub Actions <actions@github.com>
    Co-authored-by: cyrillay <cyrillay@users.noreply.github.com>
    Co-authored-by: tibz <tibo.pendino@epitech.eu>
    Co-authored-by: tibo-pdn <tibo-pdn@users.noreply.github.com>

--- api/endpoints/monitoring.py
+++ api/endpoints/monitoring.py
@@ -1,5 +1,10 @@
+import os
+
 from fastapi import Depends, FastAPI
+import prometheus_client
+from prometheus_client import CollectorRegistry, multiprocess
 from prometheus_fastapi_instrumentator import Instrumentator
+from starlette.responses import Response
 
 from api.helpers._accesscontroller import AccessController
 from api.schemas.admin.roles import PermissionType
@@ -8,10 +13,19 @@ from api.utils.variables import RouterName
 
 def setup_prometheus(app: FastAPI, include_in_schema: bool = True) -> None:
     app.instrumentator = Instrumentator().instrument(app=app)
-    app.instrumentator.expose(
-        app=app,
-        should_gzip=True,
+
+    @app.get(
+        path="/metrics",
         tags=[RouterName.MONITORING.title()],
         dependencies=[Depends(dependency=AccessController(permissions=[PermissionType.READ_METRIC]))],
         include_in_schema=include_in_schema,
     )
+    def metrics() -> Response:
+        if os.environ.get("PROMETHEUS_MULTIPROC_DIR"):
+            registry = CollectorRegistry()
+            multiprocess.MultiProcessCollector(registry)
+        else:
+            registry = prometheus_client.REGISTRY
+
+        data = prometheus_client.generate_latest(registry)
+        return Response(content=data, media_type=prometheus_client.CONTENT_TYPE_LATEST)
--- scripts/startup_api.sh
+++ scripts/startup_api.sh
@@ -3,6 +3,10 @@ set -e
 
 GUNICORN_CMD_ARGS=${GUNICORN_CMD_ARGS:-""} # ex: --log-config app/log.conf
 
+mkdir -p "$PROMETHEUS_MULTIPROC_DIR"
+# Empty prometheus dir if it exists, to prevent ghost metrics being ingested twice in case of docker restart
+rm -rf "${PROMETHEUS_MULTIPROC_DIR:?}"/*
+
 python -m alembic -c api/alembic.ini upgrade head
 
-exec gunicorn api.main:app --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 $GUNICORN_CMD_ARGS  
+exec gunicorn api.main:app --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 --config scripts/gunicorn.conf.py $GUNICORN_CMD_ARGS
